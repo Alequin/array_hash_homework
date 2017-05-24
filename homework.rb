5d55aa78feabc98849e4c4ea628b111131826cbf
James Cox <j-cox03@hotmail.com>
2017-05-24 16:30:28 +0100

    my_hash questions complete

--- homework.rb
+++ homework.rb
@@ -58,10 +58,15 @@ puts my_hash[1]
 puts my_hash[:two]
 
 # 3. How would you return the number `2`?
+puts my_hash["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
+my_hash[3] = "Three"
+p my_hash
 
 # 5. How would you add `{:four => 4}` to the hash?
+my_hash[:four] = 4
+p my_hash
 
 ### C. Given the following data structure:
 
