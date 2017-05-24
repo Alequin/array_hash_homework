2795ba95c8ceab2fc34db37405ae05f279900f2e
James Cox <j-cox03@hotmail.com>
2017-05-24 16:43:37 +0100

    Completed users Hash questions

--- homework.rb
+++ homework.rb
@@ -103,19 +103,39 @@ users = {
 }
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+puts users["Jonathan"][:twitter]
 
 # 2. Return Erik's hometown
+puts users["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
+p users["Jonathan"][:favourite_numbers]
 
 # 4. Return the type of Avril's pet Colin
+puts users["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
+puts users["Erik"][:favourite_numbers].min
 
 # 6. Add the number `7` to Erik's favorite numbers
+users["Erik"][:favourite_numbers].push(7)
+p users["Erik"][:favourite_numbers]
 
 # 7. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = 'Edinburgh'
+puts users["Erik"][:home_town]
 
 # 8. Add a pet dog to Erik called "Fluffy"
+users["Erik"][:pets]["Fluffy"] = :dog
+p users["Erik"][:pets]
 
 # 9. Add yourself to the users hash
+users["James"] = {
+  :twitter => "N/A",
+  :favourite_numbers => [7, 4, 13],
+  :home_town => "Edinburgh",
+  :pets => {
+    "Teddy" => :dog
+  }
+}
+puts users["James"]
